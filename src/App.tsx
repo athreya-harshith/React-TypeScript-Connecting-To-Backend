5904ca87a38999cafcafff50d763ea3fca2ac7e8
athreya-harshith <athreya1207@gmail.com>
2024-02-18 17:46:37 +0530

    GET HTTP request to the fake server with axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,69 @@
-import React, { useEffect, useRef, useState } from "react";
-import ProductList from "./components/ProductList";
-const connect = () => console.log("connecting");
-const disconnect = () => console.log("disconnecting");
+// import React, { useEffect, useRef, useState } from "react";
+// import ProductList from "./components/ProductList";
+// const connect = () => console.log("connecting");
+// const disconnect = () => console.log("disconnecting");
+// const App = () => {
+//   // const ref = useRef<HTMLInputElement>(null);
+//   // console.log(ref);
+
+//   // also called as after rendering
+//   // useEffect(() => {
+//   //   if (ref.current) ref.current.focus();
+//   // });
+//   // the below line has nothing to do with the dom and is a side effect hence use these in the useEffect hook
+//   //if (ref.current) ref.current.focus();
+
+//   // useEffect(() => {
+//   //   document.title = "My App";
+//   // });
+
+//   // const [category, setCategory] = useState("");
+
+//   // Effect Clean up
+//   useEffect(() => {
+//     connect(); // if this fetches the data
+
+//     return () => disconnect(); // this will abort the fetch
+//   });
+//   return (
+//     <div>
+//       {/* <input ref={ref} type="text" className="form-control" /> */}
+//       {/* <select
+//         className="form-select"
+//         onChange={(event) => setCategory(event.target.value)}
+//       >
+//         <option value=""></option>
+//         <option value="Clothing">Clothing</option>
+//         <option value="Houshold">Household</option>
+//       </select>
+//       <ProductList category={category} /> */}
+//     </div>
+//   );
+// };
+
+// export default App;
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+interface User {
+  id: number;
+  name: string;
+}
 const App = () => {
-  // const ref = useRef<HTMLInputElement>(null);
-  // console.log(ref);
-
-  // also called as after rendering
-  // useEffect(() => {
-  //   if (ref.current) ref.current.focus();
-  // });
-  // the below line has nothing to do with the dom and is a side effect hence use these in the useEffect hook
-  //if (ref.current) ref.current.focus();
-
-  // useEffect(() => {
-  //   document.title = "My App";
-  // });
-
-  // const [category, setCategory] = useState("");
-
-  // Effect Clean up
+  const [users, setUsers] = useState<User[]>([]);
   useEffect(() => {
-    connect(); // if this fetches the data
-
-    return () => disconnect(); // this will abort the fetch
-  });
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        setUsers(res.data);
+      });
+  }, []);
   return (
     <div>
-      {/* <input ref={ref} type="text" className="form-control" /> */}
-      {/* <select
-        className="form-select"
-        onChange={(event) => setCategory(event.target.value)}
-      >
-        <option value=""></option>
-        <option value="Clothing">Clothing</option>
-        <option value="Houshold">Household</option>
-      </select>
-      <ProductList category={category} /> */}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
